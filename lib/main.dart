816bba716c7accddbd1ab7e493793ccedd2b6a05
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-14 01:26:54 +0000

    Add order notes field

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,13 @@ class OrderScreen extends StatefulWidget {
 // STATE OBJECT
 class _OrderScreenState extends State<OrderScreen> {
   int _quantity = 0;
+  final TextEditingController _noteController = TextEditingController();
+
+  @override
+  void dispose() {
+    _noteController.dispose();
+    super.dispose();
+  }
 
   void _increaseQuantity() {
     if (_quantity < widget.maxQuantity) {
@@ -58,6 +65,7 @@ class _OrderScreenState extends State<OrderScreen> {
               _quantity,
               'Footlong',
             ),
+            const SizedBox(height: 24),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -65,11 +73,23 @@ class _OrderScreenState extends State<OrderScreen> {
                   onPressed: _increaseQuantity,
                   child: const Text('Add'),
                 ),
+                const SizedBox(width: 16),
                 ElevatedButton(
                   onPressed: _decreaseQuantity,
                   child: const Text('Remove'),
                 ),
               ],
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: 500,
+              child: TextField(
+                controller: _noteController,
+                decoration: const InputDecoration(
+                  hintText: 'Add special requests (e.g., "no onions")',
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ),
           ],
         ),
